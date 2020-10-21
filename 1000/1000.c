ae9407412674df008e41393c69be536e3798121e
danlee1213 <hhleeac@connect.ust.hk>
2020-10-21 23:02:12 +0900

    1000번

---
+++ 1000/1000.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+  
+
+  
+int main() { 
+ int input_a, input_b;
+ scanf("%d%d",&input_a, &input_b);
+ int addition = input_a + input_b;
+ printf("%d", addition);
+
+    return 0;
+}
